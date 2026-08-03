e5272a265e787f00a78876c1a60f432b114ad831
Gilson Melo <79469113+Gilsonmelo92@users.noreply.github.com>
2026-08-03 16:12:26 -0300

    Refatora API com HTTPException e status codes.

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,65 +1,77 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, HTTPException, status
 from app.schemas.task import TaskCreate
+
 
 router = APIRouter()
 
 tarefas = []
 
+# --- LISTAR TAREFAS ---
 @router.get("/tarefas")
-
 def listar_tarefas():
     return tarefas
 
-#buscar tarefa
-
-@router.get("/tarefas/{id}")
-
+# --- BUSCAR TAREFA ---
+@router.get("/tarefas/{id}", status_code=status.HTTP_404_NOT_FOUND)
 def buscar_tarefa(id: int):
     for tarefa in tarefas:
         if tarefa["id"] == id:
             return tarefa
 
-    return {"mensagem": "Tarefa não encontrada"}
+#    return {"mensagem": "Tarefa não encontrada"}
+#    com o importe do HTTPException, podemos retornar um erro 404 caso a tarefa não seja encontrada.
 
-#deletar tarefa
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail= "Tarefa não encontrada"
+    )
 
-@router.delete("/tarefas/{id}")
+# --- CRIAR TAREFA ---
+@router.post("/tarefas", status_code=status.HTTP_201_CREATED)
+def criar_tarefa(task: TaskCreate):
+    #Gera um novo ID baseado no maior ID existente.
+    novo_id = max([t["id"] for t in tarefas], default=0) + 1
 
-def deletar_tarefa(id: int):
-    for tarefa in tarefas:
-        if tarefa["id"] == id:
-            tarefas.remove(tarefa)
-            return {"mensagem": "Tarefa deletada com sucesso"} 
-        
-    return {"mensagem": "Tarefa não encontrada"}
-        
-#atualizar tarefa
+    nova_tarefa = {
+        "id": novo_id,
+        "titulo": task.titulo,
+        "concluida": False
+    }
 
+    tarefas.append(nova_tarefa)
+    return nova_tarefa
+
+# --- ATUALIZAR TAREFA ---
 @router.put("/tarefas/{id}")
-
 def atualizar_tarefa(id: int, task: TaskCreate):
     for tarefa in tarefas:
         if tarefa["id"] == id:
             tarefa["titulo"] = task.titulo
             return tarefa
 
-    return {"mensagem": "Tarefa não encontrada"}
+#    return {"mensagem": "Tarefa não encontrada"}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail= "Tarefa não encontrada"
+    )
 
         
+# --- DELETAR TAREFA ---
+@router.delete("/tarefas/{id}")
 
-#criar tarefa
+def deletar_tarefa(id: int):
+    for tarefa in tarefas:
+        if tarefa["id"] == id:
+            tarefas.remove(tarefa)
+            return {
+                "mensagem": "Tarefa deletada com sucesso"
+            }
 
-@router.post("/tarefas")
+#    return {"mensagem": "Tarefa não encontrada"}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail= "Tarefa não encontrada"
+    )
 
-def criar_tarefa(task: TaskCreate):
 
-    tarefa = {
-        "id": len(tarefas) + 1,
-        "titulo": task.titulo,
-        "concluida": False
-    }
-
-    tarefas.append(tarefa)
-
-    return tarefa
+        
